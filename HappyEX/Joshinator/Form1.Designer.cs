23d39539e70589bd357850ef6222d9b47b64f11f
SuperGaming69 <168477195+SuperGaming69@users.noreply.github.com>
2024-07-27 16:16:11 -0400

    added everything, imma never commit again

---
+++ HappyEX/Joshinator/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Joshinator
+{
+    partial class JoshEX
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JoshEX));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Inject = new System.Windows.Forms.Button();
+            this.Execute = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to HappyEx, made by your neighborhood skid";
+            // 
+            // Inject
+            // 
+            this.Inject.BackColor = System.Drawing.Color.Firebrick;
+            this.Inject.Location = new System.Drawing.Point(30, 455);
+            this.Inject.Name = "Inject";
+            this.Inject.Size = new System.Drawing.Size(118, 94);
+            this.Inject.TabIndex = 1;
+            this.Inject.Text = "Inject";
+            this.Inject.UseVisualStyleBackColor = false;
+            this.Inject.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Execute
+            // 
+            this.Execute.BackColor = System.Drawing.Color.Firebrick;
+            this.Execute.Location = new System.Drawing.Point(170, 455);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(118, 94);
+            this.Execute.TabIndex = 2;
+            this.Execute.Text = "Execute";
+            this.Execute.UseVisualStyleBackColor = false;
+            this.Execute.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.Firebrick;
+            this.Clear.Location = new System.Drawing.Point(318, 455);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(118, 94);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(604, 354);
+            this.textBox1.TabIndex = 4;
+            // 
+            // JoshEX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1005, 579);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.Inject);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JoshEX";
+            this.Text = "Happy Ex v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Inject;
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ HappyEX/Joshinator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Oxygen;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Joshinator
+{
+    public partial class JoshEX : Form
+    {
+        public JoshEX()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Execution.Execute(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            API.Inject();
+        }
+    }
+}
